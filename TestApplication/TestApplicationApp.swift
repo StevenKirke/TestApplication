7827a0206564965da15b052453c813ea9a394db2
Steven Kirke <Sfx.montage@gmail.com>
2020-11-02 14:42:49 +0300

    Initial Commit

---
+++ TestApplication/TestApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApplicationApp.swift
+//  TestApplication
+//
+//  Created by JARVIS on 02.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TestApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
